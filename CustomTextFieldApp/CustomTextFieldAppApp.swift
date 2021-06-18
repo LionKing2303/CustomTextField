c2ae3a1e4addc809530d3f6d237622605354e10d
Arie Peretz <ariep20@gmail.com>
2021-06-18 18:48:03 +0300

    Initial Commit

---
+++ CustomTextFieldApp/CustomTextFieldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldAppApp.swift
+//  CustomTextFieldApp
+//
+//  Created by Arie Peretz on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
